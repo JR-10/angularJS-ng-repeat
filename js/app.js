42876530411c2742f6b39f95c1923ac238a50472
Jonathan Rivera <arbeyjr@gmail.com>
2018-05-04 01:39:13 -0500

    Angularjs ng-repeat aprendiendo

--- js/app.js
+++ js/app.js
@@ -3,9 +3,28 @@
 var app = angular.module('universidadApp',[ ]);
 
 app.controller('listadoCtrl', ['$scope', function($scope){
-	
-		// creamos un array dentro del controlador
 
+    // creamos un array dentro del controlador
+    $scope.listado = ["Jonathan Rivera", "Juana Pascuala", "Florencio Cuchimba", "isabella Rivera"] ;
+
+    $scope.listadoProfesores = {
+        profesores: [
+            {
+                nombre: "Jonathan Rivera",
+                edad: 22,
+                clase: "POO"
+            },
+            {
+                nombre: "Isabella Rivera",
+                edad: 1,
+                clase: "Logica Porgrmación"
+            },
+            {
+                nombre: "Gustavo Rivera",
+                edad: 70,
+                clase: "Asebler"
+            }]
+    }
 
 
 }]);
